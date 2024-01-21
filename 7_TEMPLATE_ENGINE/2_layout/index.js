8644ed1dff5ddc98626d8aa99aa0d6c09eece377
Alexandre Amado <alexandreamado71@gmail.com>
2024-01-21 05:29:08 +0000

    finalizando com handlebars

--- 7_TEMPLATE_ENGINE/2_layout/index.js
+++ 7_TEMPLATE_ENGINE/2_layout/index.js
@@ -3,11 +3,62 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', exphbs.engine()); 
+const hbs = exphbs.create({
+    partialsDir: ["views/partials"],
+})
+
+app.engine('handlebars', hbs.engine); 
 app.set('view engine', 'handlebars'); 
 
+app.use(express.static('public'))
+
+app.get('/dashboard', (req, res) => {
+    const items = ["Item a", "Item b", "item c"]
+
+    res.render('dashboard', {items:items})
+})
+
+app.get('/post', (req, res) => {
+    const post = {
+        title: 'Aprender Node.js',
+        category: 'Javascript',
+        body: 'vais aprender a usar o nodejs...',
+        comments: 7
+    } 
+
+    res.render('blogpost', {post})
+})
+
+app.get('/blog', (req, res) => {
+    const posts = [
+        {
+        title: 'Aprender Node.js',
+        category: 'Javascript',
+        body: 'vais aprender a usar o nodejs...',
+        comments: 9
+    },
+    {
+        title: 'Aprender Node.js',
+        category: 'Javascript',
+        body: 'vais aprender a usar o nodejs...',
+        comments: 7
+    }
+]
+
+res.render('blog', {posts: posts})
+})
+
 app.get('/', (req, res) => {
-    res.render('home');
+    const user = {
+        name: "Alexandre",
+        lastName: "Amado",
+        age: 25
+    } 
+
+    const auth = true;
+    const approved = false;
+
+    res.render('home', {user: user, auth, approved});
 });
 
 app.listen(3000, () => {
